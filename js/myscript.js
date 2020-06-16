b81b84f9ea0f68f8c314e4997a8d4f31f7172d06
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-16 15:40:01 +0200

    esercizio biglietto treno terminato

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,13 @@
 // variabili
 
-var costo = 0.21 * chiLometri;
+var costo = 0.21;
+
 
 
 // quanti km devi percorrere
 
 var chiLometri = prompt ("Quanti chilometri devi percorrere?");
+
 
 
 // quanti anni hai
@@ -14,11 +16,22 @@ var eta = prompt ("Quale è la tua età?");
 
 
 
-// somma
+// calcolo del prezzo
+
+var prezzototale = chiLometri * costo;
 
 
-var somma = chiLometri + costo;
+
+// colcolo dell'eta
+
+if(eta < 18){
+  prezzototale = prezzototale / 100 * 80;
+} else if(eta > 65){
+  prezzototale = prezzototale / 100 * 60;
+}
 
 
 
-document.getElementById('title').innerHTML = "il costo del biglietto è" + somma;
+// clacolo del costo del biglietto con sconto
+
+document.getElementById('title').innerHTML = "il costo del biglietto è: " + prezzototale + " euro";
